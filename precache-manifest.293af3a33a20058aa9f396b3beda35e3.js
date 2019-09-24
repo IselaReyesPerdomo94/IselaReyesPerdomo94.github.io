6895870e6899f5d08430896441c8d280349c67be
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-09-24 16:58:26 -0500

    Updates

---
+++ precache-manifest.293af3a33a20058aa9f396b3beda35e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6a2dd6e36f016087b85471ae5ba239",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7064f7d814536fcdd118",
+    "url": "./static/css/main.439d263d.chunk.css"
+  },
+  {
+    "revision": "e30f7d9a4461b90769ac",
+    "url": "./static/js/2.5a93299f.chunk.js"
+  },
+  {
+    "revision": "7064f7d814536fcdd118",
+    "url": "./static/js/main.096b1c0f.chunk.js"
+  },
+  {
+    "revision": "fc856ed0056acf886c78",
+    "url": "./static/js/runtime-main.37ef7372.js"
+  }
+]);
