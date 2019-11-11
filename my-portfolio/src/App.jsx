5dbc0b1430362083b4f728057246d5a1b13359fd
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-11-11 17:09:30 -0600

    ANIMATION: Adding animation in the whole portfolio.

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,15 +3,17 @@ import './App.css';
 import Home from './views/Home/home';
 import Projects from './views/Projects/projects';
 import ContactView from './views/ContactView/contact';
+import Paws from './Components/Paws/paws';
 import Skills from './views/Skills/skills';
 
 function App() {
   return (
-    <div className="App">     
-    <Home/>   
-    <Skills/>
-    <Projects/>
-    <ContactView/>
+    <div className="App">   
+      <Paws/>  
+      <Home/>   
+      <Skills/>
+      <Projects/>
+      <ContactView/>
     </div>
   );
 }
