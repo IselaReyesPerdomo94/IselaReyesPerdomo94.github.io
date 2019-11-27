e539aab5a2984d8ba1df4b6f7f58bb94e04b83c9
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-11-27 14:14:47 -0600

    Updates

---
+++ precache-manifest.2b99dd81b4c113d15718f5e0ab15b7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84241ec1422f957454331a9fa228163c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a804f2847c4ddd0e998",
+    "url": "/static/css/main.760c2c51.chunk.css"
+  },
+  {
+    "revision": "2f0844d1499273d22072",
+    "url": "/static/js/2.aea13ae5.chunk.js"
+  },
+  {
+    "revision": "8a804f2847c4ddd0e998",
+    "url": "/static/js/main.a073d3c1.chunk.js"
+  },
+  {
+    "revision": "d7dc059d9dfb70300f7a",
+    "url": "/static/js/runtime-main.8b3af51d.js"
+  },
+  {
+    "revision": "07ff17135ea399103e968dd4fba7097c",
+    "url": "/static/media/photoProfile.07ff1713.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eee25da2847704d1044ae7a5fec2066d.js"
+  "/precache-manifest.2b99dd81b4c113d15718f5e0ab15b7f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
